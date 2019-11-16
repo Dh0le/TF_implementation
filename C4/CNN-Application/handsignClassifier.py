004cca977d3faa21986ffa2b4ac1e41b7bdccc0c
Dh0le <i@zjiaqi.com>
2019-11-17 00:19:25 +0800

    Complete TF2 implementation of C2W1P2

--- C4/CNN-Application/handsignClassifier.py
+++ C4/CNN-Application/handsignClassifier.py
@@ -12,16 +12,35 @@ from tensorflow import keras
 from tensorflow.python.framework import ops
 from cnn_utils import *
 
+def compute_cost(Z3, Y):
+    """
+    Computes the cost
+    
+    Arguments:
+    Z3 -- output of forward propagation (output of the last LINEAR unit), of shape (6, number of examples)
+    Y -- "true" labels vector placeholder, same shape as Z3
+    
+    Returns:
+    cost - Tensor of the cost function
+    """
+    
+    ### START CODE HERE ### (1 line of code)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Z3, labels=Y))
+    ### END CODE HERE ###
+    
+    return cost
+
 np.random.seed(1)# Loading the data (signs)
 
 ##load dataset.
 X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
 
 ## Examine the shape of data
-X_train = X_train_orig/255.
-X_test = X_test_orig/255.
+X_train = X_train_orig/255
+X_test = X_test_orig/255
 Y_train = convert_to_one_hot(Y_train_orig, 6).T
 Y_test = convert_to_one_hot(Y_test_orig, 6).T
+
 print ("The type of training sets = " + str(type(X_train)))
 print ("number of training examples = " + str(X_train.shape[0]))
 print ("number of test examples = " + str(X_test.shape[0]))
@@ -29,7 +48,31 @@ print ("X_train shape: " + str(X_train.shape))
 print ("Y_train shape: " + str(Y_train.shape))
 print ("X_test shape: " + str(X_test.shape))
 print ("Y_test shape: " + str(Y_test.shape))
-conv_layers = {}
+
+"""
+class_names = ["zero","one","two","three","four","five"]
+Y_train = [np.where(r==1)[0][0] for r in Y_train]
+Y_train = np.matrix(Y_train)
+Y_train = Y_train.T
+print(Y_train[1][0])
+
+
+## Check the data set.
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(X_train[i], cmap=plt.cm.binary)
+    # The CIFAR labels happen to be arrays, 
+    # which is why you need the extra index
+    truelablenum = np.squeeze(Y_train_orig[:, i])
+    plt.xlabel(class_names[Y_train[i][0]])
+plt.show()
+"""
+
+
 
 ## Create a model first for following layers
 model = keras.models.Sequential()
@@ -37,7 +80,7 @@ model = keras.models.Sequential()
 ## We will still keep the hyperparameter of original model where W1:[4,4,3,8] and W2:[2,2,8,16] for two conv2D layer.
 
 ##Add the first convoluted layers into the model.
-model.add(keras.layers.Conv2D(8,4,1,padding="same",activation="relu",use_bias=True,kernel_initializer=keras.initializers.glorot_normal,bias_initializer="zeros"))
+model.add(keras.layers.Conv2D(8,(4,4),padding="same",activation="relu",input_shape = (64,64,3),use_bias=True,kernel_initializer=keras.initializers.glorot_normal,bias_initializer="zeros"))
 ## Previous code completed weight bias initilization and computation towards both Z and A since activation was included. 
 
 ##Add pooling layer into model.
@@ -45,7 +88,7 @@ model.add(keras.layers.MaxPooling2D((8,8),8,padding="same"))
 ## This code will create a window for 8,8 and stride 8 pooling layer.
 
 ##Add second conv layer
-model.add(keras.layers.Conv2D(16,2,1,padding="same",activation='relu',use_bias = True,kernel_initializer=keras.initializers.glorot_normal,bias_initializer="zeros"))
+model.add(keras.layers.Conv2D(16,(2,2),padding="same",activation='relu',use_bias = True,kernel_initializer=keras.initializers.glorot_normal,bias_initializer="zeros"))
 
 ##Add second pooling layer
 model.add(keras.layers.MaxPooling2D((4,4),4,padding="same"))
@@ -53,9 +96,10 @@ model.add(keras.layers.MaxPooling2D((4,4),4,padding="same"))
 ##Add flatten layer
 model.add(keras.layers.Flatten())
 ##Add dense(FC) layer,
-model.add(keras.layers.Dense(6))
+model.add(keras.layers.Dense(6,activation = "softmax"))
+model.summary()
 
-model.compile(loss="sparse_categorical_crossentropy",
-             optimizer = "Adam"),
-              metrics=["accuracy"]
-             )
+model.compile(loss='categorical_crossentropy',
+             optimizer = "Adam",metrics=["accuracy"])
+
+model.fit(X_train,Y_train,epochs= 100,validation_data=(X_test,Y_test))
